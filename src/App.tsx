9b02b9571d4d4b84b78c452afca6c15f8368e96a
OnijYT <isroilov.asadbek0001@gmail.com>
2026-04-23 19:18:22 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './assets/header/header'
+import './styles/global.css'
+
+function App() {
+
+  return (
+    <div style={{ padding: '20px 24px' }}>
+      <Header />
+    </div>
+  )
+
+}
+
+export default App
